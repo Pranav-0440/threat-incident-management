7962e5cfb930c53c5f979c617bd0a15f74319062
Pranav-0440 <pranavghorpade61@gmail.com>
2026-09-05 00:15:45 +0530

    fix: add Jackson mixins for Spring Security User deserialization in Redis cache
    The GenericJackson2JsonRedisSerializer could not deserialize
    org.springframework.security.core.userdetails.User from Redis cache
    because it has no default constructor. Added @JsonCreator mixins for
    User and SimpleGrantedAuthority so Jackson can reconstruct cached
    UserDetails objects, fixing JWT authentication 403 errors.

--- backend/src/main/java/com/threatmgmt/config/RedisConfig.java
+++ backend/src/main/java/com/threatmgmt/config/RedisConfig.java
@@ -1,5 +1,10 @@
 package com.threatmgmt.config;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -10,20 +15,60 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.Duration;
+import java.util.Collection;
 import java.util.Map;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
+    /**
+     * Mixin that teaches Jackson how to deserialize Spring Security's User class,
+     * which has no default constructor.
+     */
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static abstract class SpringSecurityUserMixin {
+        @JsonCreator
+        SpringSecurityUserMixin(
+                @JsonProperty("username") String username,
+                @JsonProperty("password") String password,
+                @JsonProperty("authorities") Collection<? extends GrantedAuthority> authorities) {
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static abstract class SimpleGrantedAuthorityMixin {
+        @JsonCreator
+        SimpleGrantedAuthorityMixin(@JsonProperty("authority") String authority) {
+        }
+    }
+
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.addMixIn(
+                org.springframework.security.core.userdetails.User.class,
+                SpringSecurityUserMixin.class);
+        objectMapper.addMixIn(
+                SimpleGrantedAuthority.class,
+                SimpleGrantedAuthorityMixin.class);
+        objectMapper.activateDefaultTyping(
+                BasicPolymorphicTypeValidator.builder()
+                        .allowIfBaseType(Object.class)
+                        .build(),
+                ObjectMapper.DefaultTyping.NON_FINAL);
+
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(objectMapper);
+
         RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(15))
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
                 .disableCachingNullValues();
 
         Map<String, RedisCacheConfiguration> cacheConfigs = Map.of(
@@ -39,3 +84,4 @@ public class RedisConfig {
                 .build();
     }
 }
+
